eaab43e9f51d293edaf6558443ea65850338739e
M. Wiemers <github@mwmrs.de>
2026-06-23 15:01:09 +0200

    add legal notice

---
+++ src/main/java/de/mwmrs/service/LegalNoticeService.java
@@ -0,0 +1,24 @@
+package de.mwmrs.service;
+
+import de.mwmrs.dto.LegalNoticeDto;
+import de.mwmrs.entity.LegalNotice;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+public class LegalNoticeService {
+
+    public LegalNotice get() {
+        return LegalNotice.get();
+    }
+
+    @Transactional
+    public LegalNotice update(LegalNoticeDto request) {
+        LegalNotice ln = LegalNotice.get();
+        ln.name      = request.name();
+        ln.address   = request.address();
+        ln.telephone = request.telephone();
+        ln.email     = request.email();
+        return ln;
+    }
+}
